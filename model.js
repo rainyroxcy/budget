d0e8d37f8bbeb1900d003c0827ae02261a5fc660
Kalicia Adams <kally1009@kalicia-laptop.localdomain>
2021-07-08 12:42:12 -0600

    got Get method working

--- model.js
+++ model.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+const billSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    type: String,
+    amount: Number,
+    deadline: Date,
+    paid: Boolean,
+}, 
+{timestamps: true}
+);
+
+const Bill = mongoose.model("Bill", billSchema);
+
+module.exports = {
+    Bill,
+}
