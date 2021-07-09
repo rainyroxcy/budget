805dffc66f8db227d38382e796a5a5ced5dd539c
Eliza Darger <elizadarger@Elizas-MacBook-Air.local>
2021-07-08 21:06:40 -0600

    Added Add Button and CreateBill in app.js and post in server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const { Model } = require('mongoose');
-const {Bill} = require("./model");
+const express = require("express");
+const { Model } = require("mongoose");
+const { Bill } = require("./model");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json({}));
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 app.use((req, res, next) => {
   console.log(
@@ -23,20 +23,43 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/bills",(req,res)=>{
-    res.setHeader("Content-Type", "application/json");
-    console.log("Getting Bills list");
-    Bill.find({}, (err, bills) => {
-        if (err != null) {
-            res.status(500).json({
-            error: err,
-            message: "Could not list bills",
-        });
-        return;
+app.get("/bills", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log("Getting Bills list");
+  Bill.find({}, (err, bills) => {
+    if (err != null) {
+      res.status(500).json({
+        error: err,
+        message: "Could not list bills",
+      });
+      return;
     }
     res.status(200).json(bills);
-});
+  });
 });
 
+app.post("/bills", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log("Posting New Bill");
+  let creatingBill = {
+    type: req.body.type || "",
+    name: req.body.name || "",
+    description: req.body.description || "",
+    paid: req.body.paid || false,
+    deadline: req.body.deadline || new Date(),
+    amount: req.body.amount || "",
+  };
+  Bill.create(creatingBill, (err, bill) => {
+    if (err) {
+      console.log("Error. Cannot create bill");
+      res.status(500).json({
+        message: "Unable to create",
+        error: err,
+      });
+      return;
+    }
+    res.status(200).json(bill);
+  });
+});
 
 module.exports = app;
